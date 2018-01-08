59469078c4b8cd7abd544bf05ad43b8293bee7ef
chaim mallakh <haym2054@gmail.com>
2018-01-08 00:53:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function logWhisper(string ){
   console.log (string.toLowerCase())
 }
 function sayHiToGrandma(string){
-if ( string.toLowerCase( ) === string){
+if ( string.toLowerCase( ) === string)
 return "I can't hear you!"
 }
-if (string.toUpperCase( ) === string) {
+if (string.toUpperCase( ) === string) 
 return " YES INDEED! "
 }
 if (string ==="I Love You, Grandma"){
